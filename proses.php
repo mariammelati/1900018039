f012f565473d7d7e3f1f70c080c809f79c3d10bf
mariammelati <62757243+mariammelati@users.noreply.github.com>
2020-06-28 10:52:02 +0700

    Add files via upload

---
+++ proses.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title>Validasi Mendaftar</title>
+</head>
+<body>
+	<CENTER>  WELCOME	</CENTER> <br>
+    Nama Depan:<?php echo $_POST["nama-depan"];?><br>
+    Nama Belakang:<?php echo $_POST["nama-belakang"];?><br>
+    Email Anda :<?php echo $_POST["email"];?><br>
+    Kata Sandi :<?php echo $_POST["pass"];?><br>
+    Hobi :<?php echo $_POST["suka"];?><br>
+    Jenis Kelamin :<?php echo $_POST["jeniskelamin"];?><br>
+ </body>
+</html>
